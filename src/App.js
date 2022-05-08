cc9d42c0c94e8ac8305f00085a6ab34b4e704ea1
AYUSH <82461982+ayush536@users.noreply.github.com>
2022-05-08 12:09:10 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <>
+    <h1>WELCOME TO AYSUH'S GAME (jeetke dikha baske hai to)</h1>
+   <Game />
+   </>
+  );
+}
+
+export default App;
